927c541bc9c2881dafe884bd41b57fe9cca43a29
ClaudiaBiplus <claudia.filannino@gmail.com>
2024-01-23 21:57:38 +0000

    results show on page

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,25 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import SearchResults from "../SearchResults/SearchResults";
 
 
-//api call to show book recommendations
-const searchBooks = (genre) => {
-    
-    const apikey = "13c48b7534974e2cb41485d0c4dfc9a5"
-    const apiurl = "https://api.bigbookapi.com/search-books?api-key=" + apikey + "&query=" + genre;
-    
-    axios.get(apiurl)
-        .then(response => {
-           response.data.books.forEach(book => {
-            // console.log(book[0].title + ' ' + book[0].image)
-            <SearchResults />
-           })
-        })
-}
-
 //search for books depending on the button clicked
 function Search() {
+    const [books, setBooks] = useState([]);
+    const searchBooks = (genre) => {
+    
+        //api call to get book recommendations
+        const apikey = "13c48b7534974e2cb41485d0c4dfc9a5"
+        const apiurl = "https://api.bigbookapi.com/search-books?api-key=" + apikey + "&query=" + genre;
+        
+        axios.get(apiurl)
+            .then(response => {
+                setBooks(response.data.books);
+               })
+            
+    }
     const genres = ['action', 'adventure', 'anthropology', 'astronomy', 'archaeology', 'architecture', 'art', 'aviation', 'biography', 'biology', 'business', 'chemistry', 'children', 'classics', 'contemporary', 'cookbook', 'crafts', 'crime', 'dystopia', 'economics', 'education', 'engineering', 'environment', 'erotica', 'essay', 'fairy_tales', 'fantasy', 'fashion', 'feminism', 'fiction', 'finance', 'folklore', 'food', 'gaming', 'gardening', 'geography', 'geology', 'graphic_novel', 'health', 'historical', 'historical_fiction', 'history', 'horror', 'how_to', 'humor', 'inspirational', 'journalism', 'law', 'literary_fiction', 'literature', 'magical_realism', 'manga', 'martial_arts', 'mathematics', 'medicine', 'medieval', 'memoir', 'mystery', 'mythology', 'nature', 'nonfiction', 'novel', 'occult', 'paranormal', 'parenting', 'philosophy', 'physics', 'picture_book', 'poetry', 'politics', 'programming', 'psychology', 'reference', 'relationships', 'religion', 'romance', 'science_and_technology', 'science_fiction', 'self_help', 'short_stories', 'society', 'sociology', 'space', 'spirituality', 'sports', 'text_book', 'thriller', 'travel', 'true_crime', 'war', 'writing', 'young_adult'];
 
     //button name will be formatted with space and capital letters
@@ -36,6 +34,7 @@ function Search() {
                 {formatGenre(genre)}
                 </button>
             ))}
+             <SearchResults books={books} /> {/* Pass the books state as a prop to SearchResults */}
         </div>
     )
 }
--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import './style.css';
 
-function SearchResults() {
+function SearchResults({ books }) {
     return (
         <div>
-            <h1>{book[0].title}</h1>
-            <img src={book[0].image}></img>
+            {books.map((book, index) => (
+                <div key={index}>
+                    <div className="card">
+                        <img className="card-img-top" src={book[0].image}></img>
+                        <div className="card-body">
+                            <h5 className="card-title">{book[0].title}</h5>
+                            <button className="btn" onClick={() => onSave(book)}>Save</button>
+                        </div>
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
