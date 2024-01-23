3adb0f9f8aaba8f32a0de25a3081360362de8346
Hamda <nurfore14@gmail.com>
2024-01-23 18:56:48 +0000

    trying to put icons on description boxes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,14 +5,15 @@ function About() {
         <>
             <div className="row1">
                 <div className="about-card  p-5 rounded-3 m-5" style={{ width: '18rem' }}>
-                    <img src="..." className="icon" style={{ width: '200px' }} alt="icon" />
+                    <img src="\src\assets\images\book-icon.png" className="icon" style={{ width: '4rem' }} alt="icon" />
                     <div className="card-body">
                         <h5 className="card-title m-3">1</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div>
                 <div className="about-card p-5  rounded-3 m-5" style={{ width: '18rem' }}>
-                    <img src="..." className="icon" style={{ width: '200px' }} alt="icon" />
+                <a target="_blank" href="https://icons8.com/icon/12773/search">Search</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+                    <img src="src\assets\images\bookmark-icon.png" className="icon" style={{ width: '4rem' }} alt="icon" />
                     <div className="card-body">
                         <h5 className="card-title m-3">2</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -21,14 +22,14 @@ function About() {
             </div>
             <div className="row2">
                 <div className="about-card  p-5 rounded-3 m-5" style={{ width: '18rem' }}>
-                    <img src="..." className="icon" style={{ width: '200px' }} alt="icon" />
+                    <img src="src\assets\images\timer-icon.png" className="icon" style={{ width: '4rem' }} alt="icon" />
                     <div className="card-body">
                         <h5 className="card-title m-3">3</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div>
                 <div className="about-card p-5  rounded-3 m-5" style={{ width: '18rem' }}>
-                    <img src="..." className="icon" style={{ width: '200px' }} alt="icon" />
+                    <img src="src\assets\images\search-icon.png" className="icon" style={{ width: '4rem' }} alt="icon" />
                     <div className="card-body">
                         <h5 className="card-title m-3">4</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
