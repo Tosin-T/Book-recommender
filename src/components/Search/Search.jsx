43e42e9373fffb31e13e8c5d44b36c66a6983eea
ClaudiaBiplus <claudia.filannino@gmail.com>
2024-01-23 15:15:38 +0000

    updated array of genres

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -9,13 +9,13 @@ const searchBooks = (genre) => {
     const apiurl = "https://api.bigbookapi.com/search-books?api-key=" + apikey + "&query=" + genre;
     axios.get(apiurl)
         .then(response => {
-            console.log(response.data.books)
+            // console.log(response.data.books)
         })
 }
 
 
 function Search() {
-    const genres = ['action', 'food', 'paranormal']
+    const genres = ['action', 'adventure', 'anthropology', 'astronomy', 'archaeology', 'architecture', 'art', 'aviation', 'biography', 'biology', 'business', 'chemistry', 'children', 'classics', 'contemporary', 'cookbook', 'crafts', 'crime', 'dystopia', 'economics', 'education', 'engineering', 'environment', 'erotica', 'essay', 'fairy_tales', 'fantasy', 'fashion', 'feminism', 'fiction', 'finance', 'folklore', 'food', 'gaming', 'gardening', 'geography', 'geology', 'graphic_novel', 'health', 'historical', 'historical_fiction', 'history', 'horror', 'how_to', 'humor', 'inspirational', 'journalism', 'law', 'literary_fiction', 'literature', 'magical_realism', 'manga', 'martial_arts', 'mathematics', 'medicine', 'medieval', 'memoir', 'mystery', 'mythology', 'nature', 'nonfiction', 'novel', 'occult', 'paranormal', 'parenting', 'philosophy', 'physics', 'picture_book', 'poetry', 'politics', 'programming', 'psychology', 'reference', 'relationships', 'religion', 'romance', 'science_and_technology', 'science_fiction', 'self_help', 'short_stories', 'society', 'sociology', 'space', 'spirituality', 'sports', 'text_book', 'thriller', 'travel', 'true_crime', 'war', 'writing', 'young_adult'];
     return (
         <div>
             {genres.map((genre) => (
