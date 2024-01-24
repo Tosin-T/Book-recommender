e06f5d2445dbaa2986da8f73b3a248bf8492ab02
Hamda <nurfore14@gmail.com>
2024-01-24 18:44:15 +0000

    styling and trying to implement DRY

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,40 +4,38 @@ import './style.css';
 
 function Navbar() {
     return (
-        <div className="container-fluid" >
-            <div className="row flex-nowrap" >
-                <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-custom" >
+        <div className="container-fluid">
+            <div className="row flex-nowrap">
+                <div className="col-auto  bg-custom">
                     <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-5 text-black min-vh-100">
-                        <a href="/" className="d-flex align-items-center pb-3 mb-md-5 me-md-auto text-black text-decoration-none" >
+                        <Link to="/" className="d-flex align-items-center pb-3 mb-md-5 me-md-auto text-black text-decoration-none">
                             <span className="fs-1 d-none d-sm-inline">Menu</span>
-                        </a>
-                        <ul className="nav nav-pills fixed-leftq
-                         flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-                            <li className="nav-item mb-5">
-                                <div href="#" className="nav-link px-0" >
-                                    <i className="bi-house" /> <Link className="ms-1 d-none d-sm-inline home" to="/">Home</Link>
-
+                        </Link>
+                        <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                            <li className="nav-item mb-3">
+                                <div className="nav-link px-0">
+                                    <i className="bi-house-fill" /> <Link className="ms-1 d-none d-sm-inline home" to="/">Home</Link>
                                 </div>
                             </li>
 
-                            <li className="nav-item mb-5">
-                                <div href="#" className="nav-link px-0">
-                                    <i className="bi-search" /> <Link className="ms-1 d-none d-sm-inline search" to="/search">Search</Link>
+                            <li className="nav-item mb-3">
+                                <div className="nav-link px-0">
+                                    <i className="bi-search-heart" /> <Link className="ms-1 d-none d-sm-inline search" to="/search">Search</Link>
                                 </div>
                             </li>
-                            <li className="nav-item mb-5">
-                                <div href="#" className="nav-link px-0">
-                                    <i className="bi-heart" /> <Link className="ms-1 d-none d-sm-inline saved" to="/saved">Saved</Link>
+                            <li className="nav-item mb-3">
+                                <div className="nav-link px-0">
+                                    <i className="bi-heart-fill" /> <Link className="ms-1 d-none d-sm-inline saved" to="/saved">Saved</Link>
                                 </div>
                             </li>
-                            <li className="nav-item mb-5">
-                                <div href="#" className="nav-link px-0">
-                                    <i className="bi-clock" /> <Link className="ms-1 d-none d-sm-inline timer" to="/timer" >Timer</Link>
+                            <li className="nav-item mb-3">
+                                <div className="nav-link px-0">
+                                    <i className="bi bi-clock-fill" /> <Link className="ms-1 d-none d-sm-inline timer" to="/timer">Timer</Link>
                                 </div>
                             </li>
-                            <li className="nav-item mb-5">
-                                <div href="#" className="nav-link px-0">
-                                    <i className="bi-question" /> <Link className="ms-1 d-none d-sm-inline randomiser" to="/randomiser" >Random</Link>
+                            <li className="nav-item mb-3">
+                                <div className="nav-link px-0">
+                                    <i className=" bi-question-circle-fill" /> <Link className="ms-1 d-none d-sm-inline randomiser" to="/randomiser">Random</Link>
                                 </div>
                             </li>
                         </ul>
@@ -47,4 +45,5 @@ function Navbar() {
         </div>
     );
 }
+
 export default Navbar;
