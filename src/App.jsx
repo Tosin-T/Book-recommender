cbd774a11ef6b6395e8183f779589d516f013b5e
Hamda <nurfore14@gmail.com>
2024-01-25 22:00:41 +0000

    fixed file names and paths

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Wrapper from './components/Wrapper/Wrapper';
-import Logo from './components/Logo/Logo';
-
-
+import Aboutpage from './Pages/Aboutpage';
 import SearchPage from './Pages/SearchPage';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/footer';
-import Randomiser from './Pages/Randomiser';
 import Saved from './Pages/Saved';
-import Timer from './Pages/Timer';
+import TimerPage from './Pages/Timer';
 
 
 
@@ -18,18 +15,17 @@ import Timer from './Pages/Timer';
 function App() {
   return (
     <Router>
-      <Wrapper> 
+      <Wrapper>
         <Navbar />
         <Routes className='pages'>
           <Route path="*" element={<Homepage />} />
-          <Route path="/randomiser" element={<Randomiser />} />
-            <Route path="/saved" element={<Saved />} />
-            <Route path="/timer" element={<Timer />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/timer" element={<TimerPage />} />
           <Route path="/search" element={<SearchPage />} />
+          <Route path="/aboutpage" element={<Aboutpage />} />
         </Routes>
-            
-      </Wrapper>  
-      <Footer/>
+      </Wrapper>
+      <Footer />
     </Router>
   )
 
---
+++ src/components/Timer/Timer.jsx
@@ -0,0 +1,81 @@
+import { Button } from 'bootstrap'
+import { useEffect, useState } from 'react'
+import { useIdleTimer } from 'react-idle-timer'
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import Countdown from 'react-countdown';
+import './style.css'
+const TimerFunction = () => {
+    const [isTimerRunning, setTimerRunning] = useState(false);
+    const [customTimeMinutes, setCustomTimeMinutes] = useState(5); // Default custom time is 5 minutes
+    const [state, setState] = useState("It seems like you got distracted- lets take a 5 seconds and go again")
+    const [count, setCount] = useState(0)
+    const [remaining, setRemaining] = useState(0)
+    const handleStart = () => {
+        setTimerRunning(true);
+    };
+    const handleFinish = () => {
+        setTimerRunning(false);
+        // You can perform any action when the timer finishes
+        console.log('Timer finished!');
+    };
+    const handleCustomTimeChange = (event) => {
+        setCustomTimeMinutes(event.target.value);
+
+    };
+    const onIdle = () => {
+        setState("Keep up the good work you're on your way to reaching your reading Goal")
+    }
+    const onActive = () => {
+        setState("It seems like you got distracted- lets take a 5 seconds and go again")
+    }
+    const onAction = () => {
+        setCount(count + 1)
+    }
+    const { getRemainingTime } = useIdleTimer({
+        onIdle,
+        onActive,
+        onAction,
+        timeout: 5_000,
+    })
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setRemaining(Math.ceil(getRemainingTime() / 1000))
+        }, 500)
+        return () => {
+            clearInterval(interval)
+        }
+    })
+
+    return (
+        <div className="timer-container, mom">
+            <h2 className="timer-title">Reading Timer</h2>
+            <button onClick={handleStart} disabled={isTimerRunning} className="timer-button">
+                Start
+            </button>
+            <button onClick={handleFinish} disabled={!isTimerRunning} className="timer-button">
+                Finish
+            </button>
+            <div className="custom-time-container">
+                <input
+                    type="number"
+                    value={customTimeMinutes}
+                    onChange={handleCustomTimeChange}
+                    placeholder="Custom Time (minutes)"
+                />
+                <p>{state}</p>
+                <p>lets count you in {remaining}</p>
+            </div>
+            {isTimerRunning && (
+                <Countdown
+                    date={Date.now() + customTimeMinutes * 60 * 1000} // Convert minutes to milliseconds
+                    onComplete={handleFinish}
+                    renderer={({ minutes, seconds, completed, }) => (
+                        <div className="countdown">{`${minutes}:${seconds}`}</div>
+                    )}
+                />
+            )}
+        </div>
+    );
+};
+export default TimerFunction
