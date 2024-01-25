97112aac821eaa26b676062de5c04ea7e4649c6c
Hamda <nurfore14@gmail.com>
2024-01-25 20:41:47 +0000

    styled homepage with logo#

--- src/Pages/HomePage/Homepage.jsx
+++ src/Pages/HomePage/Homepage.jsx
@@ -12,11 +12,7 @@ const HomePage = () => {
   return (
     <div className="homepage">
       <div className={`logo-container ${showLogo ? "slide-in" : ""}`}>
-        <img src="src\assets\images\logo.png" alt="Logo" className="logo" />
-      </div>
-      <div className="description">
-        <h1>Welcome to Book Finder</h1>
-        <p>Find your next favorite book with ease!</p>
+        <img src="src\assets\images\text-logo.png" alt="Logo" className="logo" />
       </div>
     </div>
   );
